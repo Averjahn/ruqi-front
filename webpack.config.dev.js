da990b6da54305d057336572cceea2905ada6146
Averjahn <ogoka2001@gmail.com>
2025-10-10 20:28:48 +0300

    IsDev not defined fix 1.2

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -56,7 +56,7 @@ module.exports = () => ({
       favicon: 'src/favicon.ico',
       filename: 'index.html',
       inject: 'body',
-      templateParameters: { isDev, },
+      templateParameters: { isDev }
     }),
     new Dotenv({
       path: './.env',
