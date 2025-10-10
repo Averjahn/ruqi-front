7be4587cb0d9aec71dd1bbf3fc7e7490871512bc
Averjahn <ogoka2001@gmail.com>
2025-10-10 19:13:06 +0300

    vercel route fix + webpack optimization

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -84,4 +84,22 @@ module.exports = () => ({
     children: true,
     errorDetails: true,
   },
+  optimization: {
+    splitChunks: {
+      chunks: 'all',
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name: 'vendors',
+          chunks: 'all',
+        },
+        common: {
+          name: 'common',
+          minChunks: 2,
+          chunks: 'all',
+          enforce: true
+        }
+      }
+    }
+  }
 })
